84d27bc79b9afe44c17f00f2ec43a102b08b452b
Tiago Gueto <tiago.gueto101@gmail.com>
2023-12-06 20:09:29 -0300

    Adicionando modificações iniciaiss pra transferir o projeto para um portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home'
 import About from './components/about/About'
 import { 
-  createBrowserRouter, 
-  createRoutesFromElements, 
   Route, 
   Routes, 
-  BrowserRouter as Router,
-  RouterProvider
+  BrowserRouter as Router
 } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
   return (
       <Router>
         <Routes>
